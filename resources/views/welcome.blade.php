15280c716a45ed58ae1bd60a12a834f4c016fcde
Hendrik Tol <HendrikT@MacBook-Pro-van-Hendrik.local>
2017-11-30 14:06:00 +0100

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,179 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+        <script src="http://code.jquery.com/jquery-latest.min.js"
+        type="text/javascript"></script>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('register') }}">Register</a>
+                    @endauth
+                </div>
+            @endif
+
+            <div id="content" class="content">
+                <div class="title m-b-md">
+
+                </div>
+
+                <div class="links">
+                    <a id="js-getdata">Documentation</a>
+                    <a href="https://www.instagram.com/oauth/authorize/?client_id=55cc1ce217eb43f7a8328d3bee0f3fec&redirect_uri=http://localhost:8089&response_type=token">Laracasts</a>
+                    <a id="js-getposts">News</a>
+                    <a id="js-getlogin">Forge</a>
+                    <a id="js-getcode">GitHub</a>
+                </div>
+            </div>
+        </div>
+        <script>
+        document.getElementById('js-getposts').onclick = function(){
+           getposts();
+        };
+        document.getElementById('js-getlogin').onclick = function(){
+           getlogin();
+        };
+        document.getElementById('js-getdata').onclick = function(){
+           gettokenfromurl();
+        };
+
+        document.getElementById('js-getcode').onclick = function(){
+           return getcodefromurl();
+        };
+
+        function gettokenfromurl(){
+           var url      = window.location.href;     // Returns full URL
+           url = url.split('access_token=');
+           var accesstoken = url[1];
+           return accesstoken;
+        }
+
+        function gettokenbypost(){
+           $.ajax({
+             type: 'POST',
+             url: "http://localhost:8089/authenticate",
+
+             data: {
+                '_token': {{ csrf_field() }},
+                'code': getcodefromurl(),
+
+             },
+             error: function(){
+                console.log("no response");
+             },
+             success: function(data) {
+                var container = $('#content');
+                console.log(data);
+             }
+          });
+        }
+
+        function getcodefromurl(){
+           var url      = window.location.href;     // Returns full URL
+           url = url.split('code=');
+           var accesstoken = url[1];
+           return accesstoken;
+        }
+
+        function getposts(){
+           var accesstoken = gettokenfromurl();
+           $.ajax({
+             url: "https://api.instagram.com/v1/users/self/?access_token=" + accesstoken,
+             data: {
+                format: "json"
+             },
+             error: function(){
+                console.log("no response");
+             },
+             dataType: 'jsonp',
+             success: function(data) {
+                var container = $('#content');
+                console.log(data);
+             }
+          });
+        }
+
+        function getlogin(){
+           $.ajax({
+             url: "https://api.instagram.com/oauth/authorize/?client_id=55cc1ce217eb43f7a8328d3bee0f3fec&redirect_uri=http://localhost:8089&response_type=code",
+             dataType: 'html',
+             error: function(){
+                console.log("no response");
+             },
+             success: function(data) {
+                var container = $('#content');
+                console.log(data);
+             }
+          });
+        }
+
+        </script>
+    </body>
+</html>
